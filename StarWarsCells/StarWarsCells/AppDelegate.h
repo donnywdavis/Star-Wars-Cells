659180c485ef66644960eae6574df6484c984c32
Donny Davis <donnywdavis@icloud.com>
2016-05-04 14:58:21 -0400

    Set up project

---
+++ StarWarsCells/StarWarsCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarWarsCells
+//
+//  Created by Donny Davis on 5/4/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StarWarsCells/StarWarsCells/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StarWarsCells
+//
+//  Created by Donny Davis on 5/4/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
